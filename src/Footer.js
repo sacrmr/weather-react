1053f4cbc0bdaaf99df06469ee0a381708df2bb8
sacrmr <106908810+sacrmr@users.noreply.github.com>
2022-12-16 11:36:13 +0100

    Created Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <footer>
+        <a
+          href="https://github.com/sacrmr/weather-app"
+          target="_blank"
+          rel="noreferrer"
+          className="contact-coder"
+        >
+          Open-source code
+        </a>{" "}
+        by Samantha Cramer
+      </footer>
+    </div>
+  );
+}
